d6ae2d1d304860a9d8e3b36f02c588f500f879d3
Stefano Ranghetto <stefanoranghetto@hotmail.com>
2024-06-18 08:50:39 +0200

    modified:   NQueenSolver.c

--- NQueenSolver.c
+++ NQueenSolver.c
@@ -8,77 +8,48 @@
 // GitHub: https://github.com/stefanoranghetto
 
 long long int totalIterationCount = 0; // Total iteration counter
-long long int totalIterationsSum = 0;   // Total iterations sum to calculate the average
-time_t last_print_time; // Last time the solutions count was printed
 
 int solutionCount = 0;  // Counter for the solutions found
 
-void printSolution(int **board, int N, FILE *file) {
+void printSolution(int *cols, int N, FILE *file) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            fprintf(file, "%s ", board[i][j] ? "Q" : ".");
+            fprintf(file, "%s ", cols[i] == j ? "Q" : ".");
         }
         fprintf(file, "\n");
     }
     fprintf(file, "\n");
 }
 
-void solve(int **board, int row, int rowmask, int ldmask, int rdmask, int N, FILE *file) {
-    totalIterationsSum++;
+void solve(int *cols, int row, int rowmask, int ldmask, int rdmask, int N, FILE *file) {
     totalIterationCount++;
     int all_rows_filled = (1 << N) - 1;
 
-    // If rowmask has all bits set, it means queens have been placed successfully in all rows and the board is displayed
     if (rowmask == all_rows_filled) {
         solutionCount++;
-        fprintf(file, "Solution %d - Iterations %lld\n", solutionCount, totalIterationsSum);
-        totalIterationsSum = 0; // Reset for the next solution
-        printSolution(board, N, file);
-        time_t current_time = time(NULL);
+        fprintf(file, "Solution %d - Iterations %lld\n", solutionCount, totalIterationCount);
+        printSolution(cols, N, file);
         return;
     }
 
-    // Extract a bit mask (safe) by rowmask, ldmask, and rdmask. All set bits of 'safe' indicate the safe column index for queen placement of this iteration for row index (row).
     int safe = all_rows_filled & (~(rowmask | ldmask | rdmask));
+
     while (safe) {
-        // Extract the right-most set bit (safe column index) where the queen can be placed for this row
         int p = safe & (-safe);
         int col = __builtin_ctz(p);
-        board[row][col] = 1;
-
-        // Update rowmask, ldmask, and rdmask for the next row
-        solve(board, row + 1, rowmask | p, (ldmask | p) << 1, (rdmask | p) >> 1, N, file);
-        
-        // Reset right-most set bit to 0 so, the next iteration will continue by placing the queen at another safe column index of this row
+        cols[row] = col;
+        solve(cols, row + 1, rowmask | p, (ldmask | p) << 1, (rdmask | p) >> 1, N, file);
         safe = safe & (safe - 1);
-
-        // Backtracking, replace '1' with '0'
-        board[row][col] = 0;
     }
-    return;
 }
+
 
 void solveNQ(int N, FILE *file) {
-    int **board = (int **)malloc(N * sizeof(int *));
-    for (int i = 0; i < N; i++) {
-        board[i] = (int *)malloc(N * sizeof(int));
-        for (int j = 0; j < N; j++) {
-            board[i][j] = 0;
-        }
-    }
-
-    int row = 0;    
-    int rowmask = 0;
-    int ldmask = 0;
-    int rdmask = 0;
-
-    solve(board, row, rowmask, ldmask, rdmask, N, file);
-
-    for (int i = 0; i < N; i++) {
-        free(board[i]);
-    }
-    free(board);
+    int *cols = (int *)malloc(N * sizeof(int));
+    solve(cols, 0, 0, 0, 0, N, file);
+    free(cols);
 }
+
 
 int main() {
     printf("NQueenSolver - Copyright 2024 Stefano Ranghetto\n");
@@ -123,7 +94,7 @@ int main() {
         return 1;
     }
 
-    long long int averageIterations = totalIterationCount / solutionCount; // Calculate the average iterations
+    long long int averageIterations = solutionCount > 0 ? totalIterationCount / solutionCount : 0;
 
     fprintf(finalFile, "Number of solutions found: %d\n", solutionCount);
     fprintf(finalFile, "Number of iterations: %lld\n", totalIterationCount);
